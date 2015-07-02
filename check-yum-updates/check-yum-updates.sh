4621027cc1a9876c63c5e39305b3738bc8ef49b5
Patrik Uytterhoeven <patrik@open-future.be>
2015-07-02 13:09:23 +0200

    Added template for use

--- check-yum-updates/check-yum-updates.sh
+++ check-yum-updates/check-yum-updates.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ### Make sure yum-plugin-security package is installed ###
 ### Make sure zabbix-sender package is installed ###
-### Add the script to the crontab and import template in Zabbix ###
+### Add this script to the crontab ###
 
 HOSTNAME=$(egrep ^Hostname= /etc/zabbix/zabbix_agentd.conf | cut -d = -f 2)
 zabbixserverip=$(egrep ^ServerActive /etc/zabbix/zabbix_agentd.conf | cut -d = -f 2)
@@ -17,3 +17,4 @@ zabbix_sender -k yum.imporant -o $Important -z $zabbixserverip -s $HOSTNAME > /d
 zabbix_sender -k yum.low -o $Low -z $zabbixserverip -s $HOSTNAME > /dev/null 2>&1
 zabbix_sender -k yum.critical -o $Critical -z $zabbixserverip -s $HOSTNAME > /dev/null 2>&1
 zabbix_sender -k yum.release -o "$(cat /etc/redhat-release)" -z $zabbixserverip -s $HOSTNAME > /dev/null 2>&1
+
