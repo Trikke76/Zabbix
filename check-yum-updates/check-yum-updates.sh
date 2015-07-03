efdf2efa855cf3942d6a014b8783ffc5bb5d1b33
Patrik Uytterhoeven <patrik@open-future.be>
2015-07-03 14:06:07 +0200

    fixed typo and selinux check added

--- check-yum-updates/check-yum-updates.sh
+++ check-yum-updates/check-yum-updates.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 ### Make sure yum-plugin-security package is installed ###
-### Make sure zabbix-sender package is installed ###
-### Add this script to the crontab ###
+#ke sure zabbix-sender package is installed ###
 
+ZBX_DATA=/tmp/zabbix-sender-yum.in
 HOSTNAME=$(egrep ^Hostname= /etc/zabbix/zabbix_agentd.conf | cut -d = -f 2)
-zabbixserverip=$(egrep ^ServerActive /etc/zabbix/zabbix_agentd.conf | cut -d = -f 2)
+ZBX_SERVER_IP=$(egrep ^ServerActive /etc/zabbix/zabbix_agentd.conf | cut -d = -f 2)
 release=$(cat "/etc/redhat-release")
+enforcing=$(getenforce)
+
+if [[ "$enforcing" == "Enforcing" ]]
+then
+  selinux=1
+else
+  selinux=0
+fi
+
 
 Moderate=$(yum updateinfo list --sec-severity=Moderate | grep Moderate | wc -l)
 Important=$(yum updateinfo list --sec-severity=Important | grep Important | wc -l)
 Low=$(yum updateinfo list --sec-severity=Low | grep Low | wc -l)
 Critical=$(yum updateinfo list --sec-severity=Critical | grep Critical | wc -l)
 
-zabbix_sender -k yum.moderate -o $Moderate -z $zabbixserverip -s $HOSTNAME > /dev/null 2>&1
-zabbix_sender -k yum.imporant -o $Important -z $zabbixserverip -s $HOSTNAME > /dev/null 2>&1
-zabbix_sender -k yum.low -o $Low -z $zabbixserverip -s $HOSTNAME > /dev/null 2>&1
-zabbix_sender -k yum.critical -o $Critical -z $zabbixserverip -s $HOSTNAME > /dev/null 2>&1
-zabbix_sender -k yum.release -o "$(cat /etc/redhat-release)" -z $zabbixserverip -s $HOSTNAME > /dev/null 2>&1
+
+echo -n > $ZBX_DATA
+echo "$HOSTNAME yum.moderate $Moderate" >> $ZBX_DATA
+echo "$HOSTNAME yum.important $Important" >> $ZBX_DATA
+echo "$HOSTNAME yum.low $Low" >> $ZBX_DATA
+echo "$HOSTNAME yum.critical $Critical" >> $ZBX_DATA
+echo "$HOSTNAME yum.release $release" >> $ZBX_DATA
+echo "$HOSTNAME yum.selinux $selinux" >> $ZBX_DATA
+
+zabbix_sender -z $ZBX_SERVER_IP -i $ZBX_DATA &> /dev/null
 
